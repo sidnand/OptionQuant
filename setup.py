f39f98dbe58407cf43e40d71cca1f787e834419d
Siddharth Nand <snand233@gmail.com>
2024-11-18 19:24:44 -0800

    ready for pypi

--- setup.py
+++ setup.py
@@ -2,20 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="OptionQuant",
-    version="0.1.0",
-    description="A package for options trading strategies and analysis",
+    version="1.0.0",
     author="Siddharth Nand",
     author_email="snand233@gmail.com",
+    description="A package for options trading strategies and analysis",
+    long_description=open('README.md').read(),
+    ong_description_content_type="text/markdown",
     url="https://github.com/sidnand/OptionQuant",
     packages=find_packages(),
     install_requires=[
         "numpy",
         "matplotlib",
+        "quantlib"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.13",
 )
